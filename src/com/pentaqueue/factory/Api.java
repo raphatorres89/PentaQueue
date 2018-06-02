390a1d559e6fdbdc6129fa851a95567924630b12
Raphael <Raphael@Raphael-PC>
2018-06-01 23:37:35 -0300

    Melhorias nos itens

--- src/com/pentaqueue/factory/Api.java
+++ src/com/pentaqueue/factory/Api.java
@@ -7,7 +7,7 @@ import net.rithms.riot.api.RiotApi;
 
 public class Api {
 
-	private static final String API_KEY = "RGAPI-97048cd3-8b82-4bcf-80ca-6fb3e21afb97";
+	private static final String API_KEY = "RGAPI-8f56e520-3735-4b46-abd5-6f265b1d9d26";
 	
 	public static RiotApi getRiotApi() {
 		ApiConfig config = new ApiConfig().setDebugLevel(Level.FINEST).setKey(API_KEY);
